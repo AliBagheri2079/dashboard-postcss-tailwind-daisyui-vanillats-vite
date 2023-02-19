6ed6cc3ec2037c0045d2af89d3ead43712116a07
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-02-19 03:58:26 +0330

    :heavy_plus_sign: chore: add many postcss plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,12 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": "postcss-nesting",
+    "postcss-preset-env": {
+      features: { "nesting-rules": false },
+    },
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
 };
