3913e40504c4ca9065d5e3aa6d67ef6e5587cb49
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-02-26 02:54:02 +0330

    :sparkles: feat: create content component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,12 +1,12 @@
 import Nav from "@/layouts/private/Nav";
 import Header from "@/layouts/private/Header";
-import Routes from "./Routes";
+import Content from "./Content";
 
 const App = document.getElementById("app") as HTMLDivElement;
 App.setAttribute(
   "class",
   "grid grid-flow-row-dense grid-cols-layout grid-rows-layout md:m-5"
 );
-App.append(Nav, Header, Routes);
+App.append(Nav, Header, Content);
 
 export default App;
