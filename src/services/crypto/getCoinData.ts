690ca1326bfde4ef67967b650f638d0dbd36b954
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-03-12 01:49:45 +0330

    :sparkles: feat: create crypto api services

---
+++ src/services/crypto/getCoinData.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { type CoinDataParams } from "@/global/interface/CoinDataParams";
+import { type CoinDataType } from "@/global/interface/CoinData";
+import { APICryptoInstance } from "./instance";
+
+type CoinDataFCType = (
+  id: string,
+  params?: CoinDataParams
+) => Promise<CoinDataType | string>;
+
+const getCoinData: CoinDataFCType = async id => {
+  try {
+    const { data } = await APICryptoInstance<CoinDataType>({
+      url: `/v3/coins/${id}`,
+      params: {
+        localization: false,
+        tickers: false,
+        market_data: true,
+        community_data: false,
+        developer_data: true,
+        sparkline: true,
+      },
+    });
+    return data;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      console.log(`error message: ${error.message}`);
+      return error.message;
+    } else {
+      throw new Error(`An unexpected error occurred`);
+    }
+  }
+};
+
+export default getCoinData;
---
+++ src/services/crypto/getCoinsMarket.ts
@@ -0,0 +1,48 @@
+import { type CoinsMarketParamsType } from "@/global/interface/CoinsMarketParams";
+import { type CoinsMarketType } from "@/global/interface/CoinsMarket";
+import { APICryptoInstance } from "./instance";
+
+type CoinsMarketFCType = (
+  params: CoinsMarketParamsType
+) => Promise<{ data: CoinsMarketType[]; status: number }> | never;
+
+const getCoinsMarket: CoinsMarketFCType = async params => {
+  try {
+    const { data, status } = await APICryptoInstance({
+      url: "/v3/coins/markets",
+      params,
+    });
+    return { data, status };
+  } catch {
+    throw new Error(`Error Message from 'getCoinsMarket' API`);
+  }
+};
+
+export default getCoinsMarket;
+/*
+try {
+    // 👇️ fetch products from an API
+    const { data, status } = await axios.get<GetProductsResponse>(
+      'http://localhost:3002/products',
+      {
+        headers: {
+          Accept: 'application/json',
+        },
+      },
+    );
+
+    console.log(JSON.stringify(data, null, 4));
+
+    console.log(`response status is: ${status}`);
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(`error message: ${error.message}`);
+      return error.message;
+    } else {
+      console.log(`unexpected error: ${error}`);
+      return 'An unexpected error occurred';
+    }
+  }
+*/
