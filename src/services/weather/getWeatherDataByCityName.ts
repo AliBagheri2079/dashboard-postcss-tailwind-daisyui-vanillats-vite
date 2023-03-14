ea2e50943f547cf7124bb9f8cdf10a179399d4e3
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-03-14 03:59:36 +0330

    :sparkles: feat: create weather api services

---
+++ src/services/weather/getWeatherDataByCityName.ts
@@ -0,0 +1,21 @@
+import { weatherInstanceAPI } from "./instance";
+import { type WeatherParamsType } from "@/global/interface/WeatherParams";
+import { type WeatherDataType } from "@/global/interface/WeatherData";
+
+type WeatherDataByCityNameFCType = (
+  params: WeatherParamsType
+) => Promise<{ data: WeatherDataType; status: number }> | never;
+
+const getWeatherDataByCityName: WeatherDataByCityNameFCType = async params => {
+  try {
+    const { data, status } = await weatherInstanceAPI({
+      url: "/data/2.5/weather",
+      params,
+    });
+    return { data, status };
+  } catch {
+    throw new Error(`Error Message from 'getWeatherDataByCityName' API`);
+  }
+};
+
+export default getWeatherDataByCityName;
---
+++ src/services/weather/instance.ts
@@ -0,0 +1,10 @@
+import axios, { type AxiosInstance } from "axios";
+
+export const weatherInstanceAPI: AxiosInstance = axios.create({
+  method: "get",
+  baseURL: "https://api.openweathermap.org",
+  params: {
+    //* Your unique API key
+    appid: import.meta.env.VITE_WEATHER_API_KEY,
+  },
+});
