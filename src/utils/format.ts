3a2aff239dd951d199f6322ebf3847c07c20ec88
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-03-14 04:13:19 +0330

    :sparkles: feat: create degrees and speed formatter - add type to variables

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,14 +1,33 @@
-export const relativeTimeFormat = new Intl.RelativeTimeFormat("en", {
-  style: "narrow",
+export const relativeTimeFormat: Intl.RelativeTimeFormat =
+  new Intl.RelativeTimeFormat("en", {
+    style: "narrow",
+  });
+
+export const currencyFormat: Intl.NumberFormat = new Intl.NumberFormat(
+  "en-US",
+  {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 2,
+  }
+);
+
+export const percentageFormat: Intl.NumberFormat = new Intl.NumberFormat(
+  "en-US",
+  {
+    style: "percent",
+    signDisplay: "exceptZero",
+  }
+);
+
+export const degreesFormat: Intl.NumberFormat = new Intl.NumberFormat("en-US", {
+  style: "unit",
+  unit: "celsius",
+  notation: "compact",
+  maximumFractionDigits: 0,
 });
 
-export const currencyFormat = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-  maximumFractionDigits: 2,
-});
-
-export const percentageFormat = new Intl.NumberFormat("en-US", {
-  style: "percent",
-  signDisplay: "exceptZero",
+export const speedFormat: Intl.NumberFormat = new Intl.NumberFormat("en-US", {
+  style: "unit",
+  unit: "kilometer-per-hour",
 });
