2736b1fa77f593751943ad09a769b7302903dfd6
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-03-16 20:32:18 +0330

    :lipstick: style: change coin pie chart component styles

--- src/layouts/public/Cards/CoinPieChart/Component.ts
+++ src/layouts/public/Cards/CoinPieChart/Component.ts
@@ -1,6 +1,5 @@
 const PieChartComponent = document.createElement("div");
-PieChartComponent.className =
-  "card h-96 w-96 !flex-col bg-base-100 shadow-xl lg:card-side";
+PieChartComponent.className = "card w-full md:w-[initial] lg:card-side";
 const PieChartWrapper = document.createElement("figure");
 PieChartComponent.append(PieChartWrapper);
 
