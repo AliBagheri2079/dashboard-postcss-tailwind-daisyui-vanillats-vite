e05947b47b32cc894a2e9de44bcee90ae9b883d8
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-03-17 04:14:24 +0330

    :sparkles: feat: create notfound page

---
+++ src/pages/notfound/index.ts
@@ -0,0 +1,7 @@
+const NotFound = document.createElement("div");
+NotFound.className = "container flex items-center justify-center h-96";
+NotFound.innerHTML = `
+  <h2 class="text-8xl text-base-100">404 Page</h2>
+`;
+
+export default NotFound;
