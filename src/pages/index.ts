7fbc0340c2bf5390b8b7542fa5aacec9c30dcb3a
Ali Bagheri <alibagheri20ab79@gmail.com>
2023-04-01 03:42:25 +0430

    :memo: docs: create readme file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,17 +4,3 @@ Pages.setAttribute(
   "rounded-b-box relative col-start-2 col-end-[-1] row-span-2 bg-base-100"
 );
 export default Pages;
-
-/*
-<section>
-  <!--    
-    * commite and pre-production and release vs.1
-    * weather search with debounce & throttle - fetch & axios,
-    * list of todo with firebase,
-    * three.js
-    * login form (auth & regex) with web components and upload photo
-    * 2 language
-  -->
-  Content
-</section>
-*/
